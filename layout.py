e81410ce3d46a5658a82946757e8126b947d7b02
Dario Ribeiro <darioribeiro1998@hotmail.com>
2022-04-24 16:03:39 -0300

    v2

--- layout.py
+++ layout.py
@@ -1,79 +1,134 @@
 from tkinter import *
-
+from tkinter import ttk
+import sqlite3
 from matplotlib.pyplot import grid
 
 root = Tk()
 
-class Application():
+## Funções back-end
+class Funcs(): 
+    def limpa_tela(self):
+        self.codigo_entry.delete(0, END)
+        self.nome_entry.delete(0, END)
+        self.telefone_entry.delete(0, END)
+        self.cidade_entry.delete(0, END)
+
+    def conecta_bd(self):
+        self.conn = sqlite3.connect("clientes.bd")
+        self.cursor = self.conn.cursor(); print("Conectado ao banco de dados")
+    
+    def desconecta_bd(self):
+        self.conn.close(); print("Desconectado do banco de dados")
+    
+    def montatabelas(self):
+        self.conecta_bd(); 
+        ## Cria tabela 
+        self.cursor.execute("""
+                CREATE TABLE IF NOT EXISTS CLIENTES(
+                    COD INTERGER PRIMARY KEY,
+                    NOME_CLIENTE CHAR(40) NOT NULL,
+                    TELEFONE INTERGER(20),
+                    CIDADE CHAR(40)
+                );
+            """)
+
+        self.conn.commit(); print("Banco de dados criado")
+        self.desconecta_bd()
+
+
+class Application(Funcs):
 
     def __init__(self):
         self.root = root
         self.tela()
         self.frames_da_tela()
         self.widgets_frame1()
+        self.lista_frame2()
+        self.montatabelas()
         root.mainloop()
 
     # Redimensionamento e configs da tela
     def tela(self):
-        self.root.title("Canhotos Manual") ## Título da tela 
+        self.root.title('Criando Tela') ## Título da tela 
         self.root.configure(background='#C0C0C0') ## Cor de fundo
-        self.root.geometry("700x500") ## Tamanho que a tela será exibida inicialmente
-        self.root.resizable("true", "true") ## Permite redimensionar a tela (true/false)
+        self.root.geometry('700x500') ## Tamanho que a tela será exibida inicialmente
+        self.root.resizable('true', 'true') ## Permite redimensionar a tela (true/false)
         self.root.maxsize(width=900, height=700) ## Tamanho máximo que a tela poderá ser redimensionada  
         self.root.minsize(width=400, height=300) ## Tamanho mínimo que a tela poderá ser redimensionada 
 
     # Frames dentro da tela 
     def frames_da_tela(self):
-        self.frame_1 = Frame(self.root, bd = 4, bg="#DCDCDC", highlightbackground='#A9A9A9', highlightthickness=2) ## Bordas frame 1
-        self.frame_1.place(relx= 0.01, rely= 0.02, relwidth= 0.98, relheight= 0.46)
+        self.frame_1 = Frame(self.root, bd = 4, bg='#DCDCDC', highlightbackground='#A9A9A9', highlightthickness=2) ## Bordas frame 1
+        self.frame_1.place(relx= 0.02, rely= 0.02, relwidth= 0.96, relheight= 0.46)
 
-        self.frame_2 = Frame(self.root, bd = 4, bg="#DCDCDC", highlightbackground='#A9A9A9', highlightthickness=2) ## Bordas frame 2
-        self.frame_2.place(relx= 0.01, rely= 0.5, relwidth= 0.98, relheight= 0.46)
+        self.frame_2 = Frame(self.root, bd = 4, bg='#DCDCDC', highlightbackground='#A9A9A9', highlightthickness=2) ## Bordas frame 2
+        self.frame_2.place(relx= 0.02, rely= 0.5, relwidth= 0.96, relheight= 0.46)
 
     ## Criando botões
     def widgets_frame1(self):
         ## Botão limpar
-        self.bt_limpar = Button(self.frame_1, text="Limpar")
+        self.bt_limpar = Button(self.frame_1, text='Limpar', bd=2, bg='#696969', fg='white', font=('caribe', 8, 'bold'), command=self.limpa_tela)
         self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
         ## Botão buscar
-        self.bt_limpar = Button(self.frame_1, text="Buscar")
-        self.bt_limpar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
+        self.bt_buscar = Button(self.frame_1, text='Buscar', bd=2, bg='#696969', fg='white', font=('caribe', 8, 'bold'))
+        self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
         ## Botão novo
-        self.bt_limpar = Button(self.frame_1, text="Novo")
-        self.bt_limpar.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15) 
+        self.bt_novo = Button(self.frame_1, text='Novo', bd=2, bg='#696969', fg='white', font=('caribe', 8, 'bold'))
+        self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15) 
         ## Botão alterar
-        self.bt_limpar = Button(self.frame_1, text="Alterar")
-        self.bt_limpar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15) 
+        self.bt_alterar = Button(self.frame_1, text='Alterar', bd=2, bg='#696969', fg='white', font=('caribe', 8, 'bold'))
+        self.bt_alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15) 
         ## Botão apagar
-        self.bt_limpar = Button(self.frame_1, text="Apagar")
-        self.bt_limpar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15) 
+        self.bt_apagar = Button(self.frame_1, text='Apagar', bd=2, bg='#696969', fg='white', font=('caribe', 8, 'bold'))
+        self.bt_apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15) 
 
         ## Criação de label e entrada do código 
-        self.lb_codigo = Label(self.frame_1,text="Código")
+        self.lb_codigo = Label(self.frame_1,text='Código', bg='#DCDCDC', fg='#696969')
         self.lb_codigo.place(relx=0.05, rely=0.05)
 
         self.codigo_entry = Entry(self.frame_1)
         self.codigo_entry.place(relx=0.05, rely=0.15, relwidth=0.08)
 
         ## Crianção da label e entrada de nome 
-        self.lb_nome = Label(self.frame_1,text="Nome")
+        self.lb_nome = Label(self.frame_1,text='Nome', bg='#DCDCDC', fg='#696969')
         self.lb_nome.place(relx=0.05, rely=0.35)
 
         self.nome_entry = Entry(self.frame_1)
         self.nome_entry.place(relx=0.05, rely=0.45, relwidth=0.8)
 
         ## Crianção da label e entrada de telefone 
-        self.lb_telefone = Label(self.frame_1,text="Telefone")
+        self.lb_telefone = Label(self.frame_1,text='Telefone', bg='#DCDCDC', fg='#696969')
         self.lb_telefone.place(relx=0.05, rely=0.6)
 
         self.telefone_entry = Entry(self.frame_1)
         self.telefone_entry.place(relx=0.05, rely=0.7, relwidth=0.4)
 
         ## Crianção da label e entrada de cidade 
-        self.lb_cidade = Label(self.frame_1,text="Cidade")
+        self.lb_cidade = Label(self.frame_1,text='Cidade', bg='#DCDCDC', fg='#696969')
         self.lb_cidade.place(relx=0.5, rely=0.6)
 
         self.cidade_entry = Entry(self.frame_1)
         self.cidade_entry.place(relx=0.5, rely=0.7, relwidth=0.4)
+    
+    def lista_frame2(self):
+        self.listaCli = ttk.Treeview(self.frame_2, height=3, columns=("Col1", "Col2", "COl3", "Col4"))
+        self.listaCli.heading("#0", text="")
+        self.listaCli.heading("#1", text="Código")
+        self.listaCli.heading("#2", text="Nome")
+        self.listaCli.heading("#3", text="Telefone")
+        self.listaCli.heading("#4", text="Cidade")
+
+        self.listaCli.column("#0", width=1)
+        self.listaCli.column("#1", width=50)
+        self.listaCli.column("#2", width=200)
+        self.listaCli.column("#3", width=125)
+        self.listaCli.column("#4", width=125)
+
+        self.listaCli.place(relx=0.01, rely=0.1, relwidth=0.95, relheight=0.85)
+
+        self.scroolLista = Scrollbar(self.frame_2, orient='vertical')
+        self.listaCli.configure(yscroll=self.scroolLista.set)
+        self.scroolLista.place(relx=0.96, rely=0.1, relwidth=0.04, relheight=0.85)
+
 Application()
 
